af212f182d686d8e99c2a1371865052c68e52bec
A-Machagiri <adunurimachagiri@gmail.com>
2024-11-14 11:36:22 +0530

    Update PageObjects/Login/LoginPage_Locators.cs

--- PageObjects/Login/LoginPage_Locators.cs
+++ PageObjects/Login/LoginPage_Locators.cs
@@ -1,13 +1,9 @@
-using OpenQA.Selenium;
-
-namespace PageObjects.Login
-{
-    public static class LoginPage_Locators
-    {
-        public static By UsernameField = By.Id("user-name");
-        public static By PasswordField = By.Id("password");
-        public static By LoginButton = By.Id("login-button");
-        public static By ErrorMessage = By.CssSelector("[data-test='error']");
-        public static By ProductPageTitle = By.ClassName("title");
+namespace PageObjects.Login {
+    public static class LoginPage_Locators {
+        public static string UsernameField = "#user-name";
+        public static string PasswordField = "#password";
+        public static string LoginButton = "#login-button";
+        public static string ErrorMessage = "#error";
+        public static string ProductPageTitle = ".title";
     }
 }
