3308c84cf95504397c2893489191387acef8ab66
A-Machagiri <adunurimachagiri@gmail.com>
2024-11-14 11:26:09 +0530

    Update PageObjects/Login/LoginPage_Locators.cs

--- PageObjects/Login/LoginPage_Locators.cs
+++ PageObjects/Login/LoginPage_Locators.cs
@@ -1,13 +1,11 @@
-using OpenQA.Selenium;
-
 namespace PageObjects.Login
 {
     public static class LoginPage_Locators
     {
-        public static By UsernameField = By.Id("user-name");
-        public static By PasswordField = By.Id("password");
-        public static By LoginButton = By.Id("login-button");
-        public static By ErrorMessage = By.CssSelector("[data-test='error']");
-        public static By ProductPageTitle = By.ClassName("title");
+        public static readonly string UsernameField = "input[data-test='username']";
+        public static readonly string PasswordField = "input[data-test='password']";
+        public static readonly string LoginButton = "input[data-test='login-button']";
+        public static readonly string ErrorMessage = "h3[data-test='error']";
+        public static readonly string ProductPageTitle = "span.title"; // Locator for product page title after login
     }
 }
